d255098309b82778aa24af849e89aebc56d2a37e
caleb <cjmess01@gmail.com>
2024-08-28 15:29:22 +0000

    A

--- helper_graph.py
+++ helper_graph.py
@@ -4,16 +4,15 @@ import os
 def raw_signal(filename : str, data, showGraph): 
     if(not filename.endswith(".png")):
         filename = filename + ".png"
- #   print("MY")
+    plt.clf() 
     plt.plot(data[0], color="blue")
-  #  print("MOTHER")
+ 
     plt.plot(data[1], color="red")
-   # print("FREAKING")
+ 
     plt.title("Raw signal, channel 1 in blue, channel 2 in red")
 
     file_path = os.path.join(".", "graphs", filename)
     plt.savefig(file_path)
-    print("GOODNESS")
     if(showGraph == 'display'):
         plt.show()
     plt.clf()
